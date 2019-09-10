75a5cae2f6dc491cfe8c7bed814e2f18d4cc7913
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2019-09-10 09:02:24 +0200

    DRY

--- src/__tests__/httpClient.spec.ts
+++ src/__tests__/httpClient.spec.ts
@@ -1,98 +1,75 @@
-import nock from "nock";
+import nock, {Interceptor} from "nock";
 import Client from "../client";
 import Checkout from "../services/checkout";
 import ApiException from "../services/exception/apiException";
 import {createPaymentsCheckoutRequest} from "./checkout.spec";
 import HttpClientException from "../httpClient/httpClientException";
+import {Environment} from "../typings/enums/environment";
 
 beforeEach((): void => {
     nock.cleanAll();
 });
 
+type errorType = "HttpClientException" | "ApiException";
+type testOptions = { errorType: errorType; errorMessageContains?: string; errorMessageEquals?: string };
+
+const getResponse = async ({apiKey , environment }: { apiKey: string; environment: Environment}, cb: (scope: Interceptor) => testOptions) => {
+    const client = new Client({ apiKey, environment });
+    const checkout = new Checkout(client);
+
+    const scope = nock(`${client.config.checkoutEndpoint}/${Client.CHECKOUT_API_VERSION}`)
+        .post("/payments")
+    const { errorMessageContains, errorMessageEquals, errorType } = cb(scope);
+    const ErrorException = errorType === "ApiException" ? ApiException : HttpClientException;
+
+    try {
+        await checkout.payments(createPaymentsCheckoutRequest());
+        fail("request should fail");
+    } catch (e) {
+        expect(e instanceof ErrorException).toBeTruthy();
+        if (errorMessageEquals) {
+            expect(e.message).toEqual(errorMessageEquals);
+        }
+        if (errorMessageContains) {
+            expect(e.message.toLowerCase()).toContain(errorMessageContains);
+        }
+    }
+
+}
+
 describe("HTTP Client", function (): void {
     it("should return ApiException when no API Key is provided", async (): Promise<void> => {
-        const client = new Client({apiKey: "", environment: "TEST"});
-        const checkout = new Checkout(client);
-
-        nock(`${client.config.checkoutEndpoint}/${Client.CHECKOUT_API_VERSION}`)
-            .post("/payments")
-            .replyWithError("mocked_error_response");
-
-        try {
-            await checkout.payments(createPaymentsCheckoutRequest());
-            fail("request should fail");
-        } catch (e) {
-            expect(e instanceof ApiException).toBeTruthy();
-            expect(e.message.toLowerCase()).toContain("x-api-key");
-        }
+        await getResponse({ apiKey: "", environment: "TEST" }, (scope) => {
+            scope.replyWithError("mocked_error_response");
+            return { errorType: "ApiException", errorMessageContains: "x-api-key" };
+        });
     });
 
     it("should return ApiException on request error", async (): Promise<void> => {
-        const client = new Client({apiKey: "MOCKED_API_KEY", environment: "TEST"});
-        const checkout = new Checkout(client);
-
-        nock(`${client.config.checkoutEndpoint}/${Client.CHECKOUT_API_VERSION}`)
-            .post("/payments")
-            .replyWithError("some_error");
-
-        try {
-            await checkout.payments(createPaymentsCheckoutRequest());
-            fail("request should fail");
-        } catch (e) {
-            expect(e instanceof ApiException).toBeTruthy();
-            expect(e.message.toLowerCase()).toEqual("some_error");
-        }
+        await getResponse({ apiKey: "", environment: "TEST" }, (scope) => {
+            scope.replyWithError("");
+            return { errorType: "ApiException", errorMessageContains: "x-api-key" };
+        });
     });
 
     it("should return HttpClientException on invalid request", async (): Promise<void> => {
-        const client = new Client({apiKey: "API_KEY", environment: "TEST"});
-        const checkout = new Checkout(client);
-        const error = { status: 401, message: "Invalid Request", errorCode: "171", errorType: "validationError"};
-
-        nock(`${client.config.checkoutEndpoint}/${Client.CHECKOUT_API_VERSION}`)
-            .post("/payments")
-            .reply(401, error);
-
-        try {
-            await checkout.payments(createPaymentsCheckoutRequest());
-            fail("request should fail");
-        } catch (e) {
-            expect(e instanceof HttpClientException).toBeTruthy();
-            expect(e.message).toEqual("HTTP Exception: 401. null: Invalid Request");
-        }
+        await getResponse({ apiKey: "API_KEY", environment: "TEST" }, (scope) => {
+            scope.reply(401, { status: 401, message: "Invalid Request", errorCode: "171", errorType: "validationError"});
+            return { errorType: "HttpClientException", errorMessageEquals: "HTTP Exception: 401. null: Invalid Request" };
+        });
     });
 
     it("should return HttpClientException on invalid that is not an ApiError type", async (): Promise<void> => {
-        const client = new Client({apiKey: "API_KEY", environment: "TEST"});
-        const checkout = new Checkout(client);
-
-        nock(`${client.config.checkoutEndpoint}/${Client.CHECKOUT_API_VERSION}`)
-            .post("/payments")
-            .reply(401, {});
-
-        try {
-            await checkout.payments(createPaymentsCheckoutRequest());
-            fail("request should fail");
-        } catch (e) {
-            expect(e instanceof HttpClientException).toBeTruthy();
-            expect(e.message).toEqual("HTTP Exception: 401. null");
-        }
+        await getResponse({ apiKey: "API_KEY", environment: "TEST" }, (scope) => {
+            scope.reply(401, {});
+            return { errorType: "HttpClientException", errorMessageEquals: "HTTP Exception: 401. null" };
+        });
     });
 
-    it("should return ApiException on invalid string error", async (): Promise<void> => {
-        const client = new Client({apiKey: "API_KEY", environment: "TEST"});
-        const checkout = new Checkout(client);
-
-        nock(`${client.config.checkoutEndpoint}/${Client.CHECKOUT_API_VERSION}`)
-            .post("/payments")
-            .reply(401, "fail");
-
-        try {
-            await checkout.payments(createPaymentsCheckoutRequest());
-            fail("request should fail");
-        } catch (e) {
-            expect(e instanceof HttpClientException).toBeTruthy();
-            expect(e.message).toEqual("HTTP Exception: 401. null");
-        }
+    it("should return HttpClientException on invalid string error", async (): Promise<void> => {
+        await getResponse({ apiKey: "API_KEY", environment: "TEST" }, (scope) => {
+            scope.reply(401, "fail");
+            return { errorType: "HttpClientException", errorMessageEquals: "HTTP Exception: 401. null" };
+        });
     });
 });
