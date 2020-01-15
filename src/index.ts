a3ebb01aeea1ede4ce6b07e1f422c9666f4b49c6
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-01-15 15:55:02 +0100

    change types ext

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,28 @@
  * See the LICENSE file for more info.
  */
 
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="typings/amount.ts" />
+/// <reference path="typings/apiError.ts" />
+/// <reference path="typings/applicationInfo.ts" />
+/// <reference path="typings/binLookup.ts" />
+/// <reference path="typings/checkout.ts" />
+/// <reference path="typings/checkoutUtility.ts" />
+/// <reference path="typings/enums/environment.ts" />
+/// <reference path="typings/enums/vatCategory.ts" />
+/// <reference path="typings/marketPayAccount.ts" />
+/// <reference path="typings/marketPayFund.ts" />
+/// <reference path="typings/marketPayHostedOnboardingPage.ts" />
+/// <reference path="typings/marketPayNotificationConfiguration.ts" />
+/// <reference path="typings/marketPayNotifications.ts" />
+/// <reference path="typings/nexo.ts" />
+/// <reference path="typings/notification.ts" />
+/// <reference path="typings/payments.ts" />
+/// <reference path="typings/payouts.ts" />
+/// <reference path="typings/recurring.ts" />
+/// <reference path="typings/requestOptions.ts" />
+/// <reference path="typings/terminal.ts" />
+
 export { default as Client } from "./client";
 export { default as Config } from "./config";
 export * from "./services/";
