05a182e25600825f78f43ce2823afe6d6332b4c1
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-05-01 09:58:45 +0200

    terminal e2e

--- src/__mocks__/terminalApi/sync.ts
+++ src/__mocks__/terminalApi/sync.ts
@@ -4,10 +4,10 @@ export const syncRes = JSON.stringify({
             MessageCategory: "Payment",
             MessageClass: "Service",
             MessageType: "Response",
-            POIID: "P400Plus-123456789",
+            POIID: process.env.ADYEN_TERMINAL_POIID,
             ProtocolVersion: "3.0",
             SaleID: "001",
-            ServiceID: "1234567890",
+            ServiceID: "001",
         },
         PaymentResponse: {
             POIData: {
