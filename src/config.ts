e2bb647e1307ba6b714248a4271c959270aa5058
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-11-13 14:51:06 +0100

    Release v6.0.2 (#591)
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
    Co-authored-by: fredriksmaamo <fredrik.smaamo@sonat.no>
    Co-authored-by: Fredrik <46444770+smafre@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,6 @@
 interface ConfigConstructor {
     username?: string;
     password?: string;
-    merchantAccount?: string;
     environment?: Environment;
     endpoint?: string;
     marketPayEndpoint?: string;
@@ -39,7 +38,6 @@ interface ConfigConstructor {
 class Config {
     public username?: string;
     public password?: string;
-    public merchantAccount?: string;
     public environment?: Environment;
     public endpoint?: string;
     public marketPayEndpoint?: string;
@@ -61,7 +59,6 @@ class Config {
     public constructor(options: ConfigConstructor = {}) {
         if (options.username) this.username = options.username;
         if (options.password) this.password = options.password;
-        if (options.merchantAccount) this.merchantAccount = options.merchantAccount;
         if (options.environment) this.environment = options.environment;
         if (options.endpoint) this.endpoint = options.endpoint;
         if (options.marketPayEndpoint) this.marketPayEndpoint = options.marketPayEndpoint;
