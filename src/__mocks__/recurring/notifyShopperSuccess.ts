32c40b652d12f504bb52deda6e95b5785941b3c5
wouterboe <wouter.boereboom@adyen.com>
2021-05-04 13:21:20 +0200

    added notifyShopper endpoint to recurring API service

---
+++ src/__mocks__/recurring/notifyShopperSuccess.ts
@@ -0,0 +1,28 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ * Adyen NodeJS API Library
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+export const notifyShopperSuccess = {
+    "message": "Request Processed Successfully",
+    "resultCode": "Success",
+    "shopperNotificationReference": "9915003646742627",
+    "storedPaymentMethodId": "8415995487234100",
+    "pspReference": "9915003646742627",
+    "reference": "Example reference",
+    "displayedReference": "Example displayed reference"
+};
