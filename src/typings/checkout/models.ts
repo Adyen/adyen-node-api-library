2c34316e04c61a7e354a6d92144f9d9f95c6b57a
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-07-30 17:00:37 +0200

    added file headers to all typings files

--- src/typings/checkout/models.ts
+++ src/typings/checkout/models.ts
@@ -16,6 +16,25 @@
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more info.
  */
+ 
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ * Adyen NodeJS API Library
+ * Copyright (c) 2021 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
 
 export * from './accountInfo';
 export * from './achDetails';
