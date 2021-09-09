4dc114f7ee3510ad875ea18abbd85b885fd4d7c4
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-09-09 15:47:05 +0200

    added type exports to root file

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@
  *                               #############
  *                               ############
  * Adyen NodeJS API Library
- * Copyright (c) 2020 Adyen B.V.
+ * Copyright (c) 2021 Adyen B.V.
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more info.
  */
@@ -24,3 +24,17 @@ export { default as Config } from "./config";
 export * from "./services/";
 export { hmacValidator } from "./utils";
 export { default as HttpURLConnectionClient } from "./httpClient/httpURLConnectionClient";
+
+import * as checkoutTypes from './typings/checkout/models';
+import * as recurringTypes from './typings/recurring/models';
+import * as notificationTypes from './typings/notification/models';
+import * as platformAccountTypes from './typings/platformsAccount/models';
+import * as terminalTypes from './typings/terminal/models';
+export namespace Types {
+    export import checkout = checkoutTypes;
+    export import recurring = recurringTypes;
+    export import notification = notificationTypes;
+    export import platformsAccount = platformAccountTypes;
+    export import terminal = terminalTypes;
+};
+
