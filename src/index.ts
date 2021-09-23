0a524459923c471826ceedde91a49f33140a4f14
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-09-23 11:57:21 +0200

    changed structure of typing imports thus fixing linting errors

--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,4 @@ export { default as Config } from "./config";
 export * from "./services/";
 export { hmacValidator } from "./utils";
 export { default as HttpURLConnectionClient } from "./httpClient/httpURLConnectionClient";
-
-import * as checkoutTypes from './typings/checkout/models';
-import * as recurringTypes from './typings/recurring/models';
-import * as notificationTypes from './typings/notification/models';
-import * as platformAccountTypes from './typings/platformsAccount/models';
-import * as terminalTypes from './typings/terminal/models';
-export namespace Types {
-    export import checkout = checkoutTypes;
-    export import recurring = recurringTypes;
-    export import notification = notificationTypes;
-    export import platformsAccount = platformAccountTypes;
-    export import terminal = terminalTypes;
-};
-
+export * as Types from "./typings";
