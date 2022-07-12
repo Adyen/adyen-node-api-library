bfde2c532b778ab8e469717eaf52a2e5ffb34b2a
wouterboe <wouter.boereboom@adyen.com>
2022-07-12 09:50:21 +0200

    update typings index

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,22 +1,3 @@
-/*
- *                       ######
- *                       ######
- * ############    ####( ######  #####. ######  ############   ############
- * #############  #####( ######  #####. ######  #############  #############
- *        ######  #####( ######  #####. ######  #####  ######  #####  ######
- * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
- * ###### ######  #####( ######  #####. ######  #####          #####  ######
- * #############  #############  #############  #############  #####  ######
- *  ############   ############  #############   ############  #####  ######
- *                                      ######
- *                               #############
- *                               ############
- * Adyen NodeJS API Library
- * Copyright (c) 2021 Adyen B.V.
- * This file is open source and available under the MIT license.
- * See the LICENSE file for more info.
- */
-
 /// <reference path="amount.ts" />
 /// <reference path="apiError.ts" />
 /// <reference path="applicationInfo.ts" />
@@ -28,12 +9,12 @@
 /// <reference path="payouts.ts" />
 /// <reference path="requestOptions.ts" />
 /// <reference path="platformsFund.ts" />
-/// <reference path="platformsHostedOnboardingPage.ts" />
 /// <reference path="platformsNotificationConfiguration.ts" />
 
 export * as checkout from './checkout/models';
 export * as notification from './notification/models';
 export * as platformsNotifications from './platformsNotifications/models';
 export * as platformsAccount from './platformsAccount/models';
+export * as platformsHostedOnboardingPage from './platformsHostedOnboardingPage/models';
 export * as recurring from './recurring/models'
 export * as terminal from './terminal/models';
