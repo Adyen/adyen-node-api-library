39ae8bb9c1d494ffe9539f9fcab483efc6f7dbcb
michaelpa <michael.paul@adyen.com>
2022-09-16 11:06:07 +0200

    PW-6847: Mocks

---
+++ src/__mocks__/legalEntityManagement/responses.ts
@@ -0,0 +1,241 @@
+export const businessLine = {
+    "capability": "string",
+    "id": "string",
+    "industryCode": "string",
+    "legalEntityId": "string",
+    "salesChannels": ["string"],
+    "sourceOfFunds": {
+        "acquiringBusinessLineId": "string",
+        "adyenProcessedFunds": false,
+        "description": "string",
+        "type": "business"
+    },
+    "webData": [{ "webAddress": "string" }],
+    "webDataExemption": { "reason": "noOnlinePresence" }
+};
+
+export const document = {
+    "attachment": {
+        "content": "string",
+        "contentType": "string",
+        "filename": "string",
+        "pageName": "string",
+        "pageType": "string"
+    },
+    "attachments": [{
+        "content": "string",
+        "contentType": "string",
+        "filename": "string",
+        "pageName": "string",
+        "pageType": "string"
+    }],
+    "description": "string",
+    "expiryDate": "string",
+    "fileName": "string",
+    "id": "string",
+    "issuerCountry": "string",
+    "issuerState": "string",
+    "number": "string",
+    "owner": {
+        "id": "string",
+        "type": "string"
+    },
+    "type": "bankStatement"
+};
+
+export const onboardingLink = { "url": "string" };
+
+export const onboardingThemes = {
+    "next": "string",
+    "previous": "string",
+    "themes": [{
+        "createdAt": "string",
+        "description": "string",
+        "id": "string",
+        "properties": { "sample": "string" },
+        "updatedAt": "string"
+    }]
+};
+
+export const onboardingTheme = {
+    "createdAt": "string",
+    "description": "string",
+    "id": "string",
+    "properties": { "sample": "string" },
+    "updatedAt": "string"
+};
+
+export const legalEntity = {
+    "documentDetails": [{
+        "active": false,
+        "fileName": "string",
+        "id": "string"
+    }],
+    "documents": [{ "id": "string" }],
+    "entityAssociations": [{
+        "associatorId": "string",
+        "entityType": "string",
+        "jobTitle": "string",
+        "legalEntityId": "string",
+        "name": "string",
+        "type": "signatory"
+    }],
+    "id": "string",
+    "individual": {
+        "birthData": { "dateOfBirth": "string" },
+        "email": "string",
+        "identificationData": {
+            "expiryDate": "string",
+            "issuerCountry": "string",
+            "issuerState": "string",
+            "nationalIdExempt": false,
+            "number": "string",
+            "type": "bankStatement"
+        },
+        "name": {
+            "firstName": "string",
+            "infix": "string",
+            "lastName": "string"
+        },
+        "nationality": "string",
+        "phone": {
+            "number": "string",
+            "type": "string"
+        },
+        "residentialAddress": {
+            "city": "string",
+            "country": "string",
+            "postalCode": "string",
+            "stateOrProvince": "string",
+            "street": "string",
+            "street2": "string"
+        },
+        "taxInformation": [{
+            "country": "string",
+            "number": "string",
+            "type": "string"
+        }],
+        "webData": { "webAddress": "string" }
+    },
+    "organization": {
+        "description": "string",
+        "doingBusinessAs": "string",
+        "email": "string",
+        "legalName": "string",
+        "phone": {
+            "number": "string",
+            "type": "string"
+        },
+        "principalPlaceOfBusiness": {
+            "city": "string",
+            "country": "string",
+            "postalCode": "string",
+            "stateOrProvince": "string",
+            "street": "string",
+            "street2": "string"
+        },
+        "registeredAddress": {
+            "city": "string",
+            "country": "string",
+            "postalCode": "string",
+            "stateOrProvince": "string",
+            "street": "string",
+            "street2": "string"
+        },
+        "registrationNumber": "string",
+        "stockData": {
+            "marketIdentifier": "string",
+            "stockNumber": "string",
+            "tickerSymbol": "string"
+        },
+        "taxInformation": [{
+            "country": "string",
+            "number": "string",
+            "type": "string"
+        }],
+        "taxReportingClassification": {
+            "businessType": "other",
+            "financialInstitutionNumber": "string",
+            "mainSourceOfIncome": "businessOperation",
+            "type": "nonFinancialNonReportable"
+        },
+        "type": "associationIncorporated",
+        "vatAbsenceReason": "industryExemption",
+        "vatNumber": "string",
+        "webData": { "webAddress": "string" }
+    },
+    "reference": "string",
+    "soleProprietorship": {
+        "countryOfGoverningLaw": "string",
+        "doingBusinessAs": "string",
+        "name": "string",
+        "principalPlaceOfBusiness": {
+            "city": "string",
+            "country": "string",
+            "postalCode": "string",
+            "stateOrProvince": "string",
+            "street": "string",
+            "street2": "string"
+        },
+        "registeredAddress": {
+            "city": "string",
+            "country": "string",
+            "postalCode": "string",
+            "stateOrProvince": "string",
+            "street": "string",
+            "street2": "string"
+        },
+        "registrationNumber": "string",
+        "vatAbsenceReason": "industryExemption",
+        "vatNumber": "string"
+    },
+    "transferInstruments": [{ "id": "string" }],
+    "type": "individual"
+};
+
+export const businessLines = {
+    "businessLines": [{
+        "capability": "string",
+        "id": "string",
+        "industryCode": "string",
+        "legalEntityId": "string",
+        "salesChannels": ["string"],
+        "sourceOfFunds": {
+            "acquiringBusinessLineId": "string",
+            "adyenProcessedFunds": false,
+            "description": "string",
+            "type": "business"
+        },
+        "webData": [{ "webAddress": "string" }],
+        "webDataExemption": { "reason": "noOnlinePresence" }
+    }]
+};
+
+export const transferInstrument = {
+    "bankAccount": {
+        "accountNumber": "string",
+        "accountType": "string",
+        "bankBicSwift": "string",
+        "bankCity": "string",
+        "bankCode": "string",
+        "bankName": "string",
+        "branchCode": "string",
+        "checkCode": "string",
+        "countryCode": "string",
+        "currencyCode": "string",
+        "iban": "string"
+    },
+    "documentDetails": [{
+        "active": false,
+        "fileName": "string",
+        "id": "string"
+    }],
+    "id": "string",
+    "legalEntityId": "string",
+    "recurringDetail": {
+        "merchantAccount": "string",
+        "reference": "string",
+        "shopperReference": "string"
+    },
+    "type": "bankAccount"
+};
