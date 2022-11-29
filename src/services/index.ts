82d49276d1f7969a4a345991f4b7c7d9b479d004
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2022-11-29 10:00:20 +0100

    fix: add balancePlatform to services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -11,3 +11,4 @@ export { default as TerminalManagement} from "./terminalManagement";
 export { default as Management } from "./management";
 export { default as LegalEntityManagement } from "./legalEntityManagement";
 export { default as Transfers } from "./transfers";
+export { default as BalancePlatform } from "./balancePlatform";
