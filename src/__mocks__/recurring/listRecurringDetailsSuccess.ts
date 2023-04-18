5e46a73a51a0e1f7f5ca2fd708be856809546f6a
Wouter Boereboom <wouter.boereboom@adyen.com>
2023-04-18 14:59:33 +0200

    add recurring wrapper to unit tests

--- src/__mocks__/recurring/listRecurringDetailsSuccess.ts
+++ src/__mocks__/recurring/listRecurringDetailsSuccess.ts
@@ -1,77 +1,287 @@
-/*
- *                       ######
- *                       ######
- * ############    ####( ######  #####. ######  ############   ############
- * #############  #####( ######  #####. ######  #############  #############
- *        ######  #####( ######  #####. ######  #####  ######  #####  ######
- * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
- * ###### ######  #####( ######  #####. ######  #####          #####  ######
- * #############  #############  #############  #############  #####  ######
- *  ############   ############  #############   ############  #####  ######
- *                                      ######
- *                               #############
- *                               ############
- * Adyen NodeJS API Library
- * Copyright (c) 2020 Adyen B.V.
- * This file is open source and available under the MIT license.
- * See the LICENSE file for more info.
- */
-
 export const listRecurringDetailsSuccess = {
-    "creationDate": "2017-03-01T11:53:11+01:00",
+    "creationDate": "2023-02-14T13:16:53+01:00",
     "details": [
-        {
-            "acquirer": "TestPmmAcquirer",
-            "acquirerAccount": "TestPmmAcquirerAccount",
-            "additionalData": {
-                "cardBin": "411111"
-            },
-            "alias": "cardAlias",
-            "aliasType": "Default",
-            "card": {
-                "expiryMonth": "8",
-                "expiryYear": "2018",
-                "holderName": "Holder",
-                "number": "1111"
-            },
-            "contractTypes": [
-                "ONECLICK"
-            ],
-            "creationDate": "2017-03-07T09:43:33+01:00",
-            "firstPspReference": "8524888762135795",
-            "paymentMethodVariant": "visa",
-            "recurringDetailReference": "recurringReference",
-            "variant": "visa"
-        },
-        {
-            "acquirer": "PayPalSandbox",
-            "acquirerAccount": "TestPmmAcquirerAccount",
-            "billingAddress": {
-                "city": "City",
-                "country": "NL",
-                "houseNumberOrName": "1",
-                "postalCode": "2312aa",
-                "stateOrProvince": "NA",
-                "street": "Street"
-            },
-            "contractTypes": [
-                "RECURRING"
-            ],
-            "creationDate": "2017-10-10T08:50:02+02:00",
-            "firstPspReference": "8515076181707110",
-            "paymentMethodVariant": "paypal",
-            "recurringDetailReference": "8315076181982020",
-            "tokenDetails": {
-                "tokenData": {
-                    "EmailId": "tedtest@test.nl",
-                    "PayPal.PayerId": "H95EPL8B2KFE6",
-                    "BillingAgreementId": "B-7MA42752FE774625C"
-                },
-                "tokenDataType": "PayPal"
-            },
-            "variant": "paypal"
+      {
+        "RecurringDetail": {
+          "additionalData": {
+            "cardBin": "498843"
+          },
+          "alias": "C855548920309858",
+          "aliasType": "Default",
+          "billingAddress": {
+            "city": "So Paulo",
+            "country": "BR",
+            "houseNumberOrName": "999",
+            "postalCode": "59000060",
+            "stateOrProvince": "SP",
+            "street": "Roque Petroni Jr"
+          },
+          "card": {
+            "expiryMonth": "3",
+            "expiryYear": "2030",
+            "holderName": "Checkout Shopper PlaceHolder",
+            "number": "4305"
+          },
+          "contractTypes": [
+            "RECURRING"
+          ],
+          "creationDate": "2023-02-16T16:46:20+01:00",
+          "firstPspReference": "NLHK9HHRHV5X8N82",
+          "name": "YOUR_ORDER_NUMBER",
+          "networkTxReference": "452521998328201",
+          "paymentMethodVariant": "visacredit",
+          "recurringDetailReference": "HW9LL9ML73M84H82",
+          "variant": "visa"
         }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "bankName": "Wirecard",
+            "countryCode": "BE",
+            "iban": "DE87123456781234567890",
+            "ownerName": "Simon Hopper"
+          },
+          "contractTypes": [
+            "PAYOUT",
+            "RECURRING"
+          ],
+          "creationDate": "2023-02-28T12:39:00+01:00",
+          "firstPspReference": "D9CH2G934CLZNN82",
+          "paymentMethodVariant": "bankTransfer_IBAN",
+          "recurringDetailReference": "XH7XND39N6KXWD82",
+          "shopperName": {
+            "firstName": "Adyen",
+            "lastName": "Test"
+          },
+          "variant": "bankTransfer_IBAN"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "countryCode": "BE",
+            "iban": "BE46736048759136",
+            "ownerName": "Simon Hopper"
+          },
+          "contractTypes": [
+            "PAYOUT",
+            "RECURRING"
+          ],
+          "creationDate": "2023-02-28T12:41:58+01:00",
+          "firstPspReference": "W7FW5B2H8JHKGK82",
+          "paymentMethodVariant": "bankTransfer_IBAN",
+          "recurringDetailReference": "WZ6H5G39N6KXWD82",
+          "shopperName": {
+            "firstName": "Adyen",
+            "lastName": "Test"
+          },
+          "variant": "bankTransfer_IBAN"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "bic": "KREDBEBB",
+            "countryCode": "BE",
+            "iban": "BE46736048759136",
+            "ownerName": "Simon Hopper"
+          },
+          "contractTypes": [
+            "RECURRING"
+          ],
+          "creationDate": "2023-02-28T12:42:26+01:00",
+          "firstPspReference": "JVJTJB2H8JHKGK82",
+          "paymentMethodVariant": "sepadirectdebit_received",
+          "recurringDetailReference": "RX62K88Z73M84H82",
+          "tokenDetails": {
+            "tokenData": {
+              "mandateId": "GJXTXLM8P4BLNK82",
+              "dateOfSignature": "2023-02-28"
+            },
+            "tokenDataType": "SepaDirectDebit"
+          },
+          "variant": "sepadirectdebit"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "bankName": "Wirecard",
+            "countryCode": "RO",
+            "iban": "DE87123456781234567890",
+            "ownerName": "Simon Hopper"
+          },
+          "contractTypes": [
+            "RECURRING",
+            "PAYOUT"
+          ],
+          "creationDate": "2023-03-20T07:02:06+01:00",
+          "firstPspReference": "TNTXL2R576ZW8N82",
+          "paymentMethodVariant": "bankTransfer_IBAN",
+          "recurringDetailReference": "QJ9SVJRVN6KXWD82",
+          "shopperName": {
+            "firstName": "Adyen",
+            "lastName": "Test"
+          },
+          "variant": "bankTransfer_IBAN"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "bankName": "Wirecard",
+            "countryCode": "RO",
+            "iban": "NL54INGB0796686335",
+            "ownerName": "Simon Hopper"
+          },
+          "contractTypes": [
+            "RECURRING",
+            "PAYOUT"
+          ],
+          "creationDate": "2023-03-20T07:04:53+01:00",
+          "firstPspReference": "F9Z2JWLGKWD2WN82",
+          "paymentMethodVariant": "bankTransfer_IBAN",
+          "recurringDetailReference": "ZKTZFDXL83M84H82",
+          "shopperName": {
+            "firstName": "Adyen",
+            "lastName": "Test"
+          },
+          "variant": "bankTransfer_IBAN"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "bankName": "Ideal bridge test issuer",
+            "bic": "TESTNL01",
+            "countryCode": "NL",
+            "iban": "NL13TEST0123456789",
+            "ownerName": "A. Klaassen"
+          },
+          "billingAddress": {
+            "city": "So Paulo",
+            "country": "BR",
+            "houseNumberOrName": "999",
+            "postalCode": "59000060",
+            "stateOrProvince": "SP",
+            "street": "Roque Petroni Jr"
+          },
+          "contractTypes": [
+            "RECURRING"
+          ],
+          "creationDate": "2023-03-23T13:33:21+01:00",
+          "firstPspReference": "SWRXWHN79JHKGK82",
+          "paymentMethodVariant": "idealtesttoolissuer",
+          "recurringDetailReference": "C7MLCNPZN6KXWD82",
+          "variant": "ideal"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "bankName": "Wirecard",
+            "bic": "TESTDE01",
+            "countryCode": "DE",
+            "iban": "DE87123456781234567890",
+            "ownerName": "Simon Hopper"
+          },
+          "contractTypes": [
+            "RECURRING"
+          ],
+          "creationDate": "2023-03-30T16:00:15+02:00",
+          "firstPspReference": "FF944F5DFQHG5S82",
+          "paymentMethodVariant": "sepadirectdebit_received",
+          "recurringDetailReference": "C8DBLV28P6KXWD82",
+          "tokenDetails": {
+            "tokenData": {
+              "mandateId": "W3MLWQ335CLZNN82",
+              "dateOfSignature": "2023-03-30"
+            },
+            "tokenDataType": "SepaDirectDebit"
+          },
+          "variant": "sepadirectdebit"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "additionalData": {
+            "cardBin": "411111"
+          },
+          "alias": "K333136193308394",
+          "aliasType": "Default",
+          "card": {
+            "expiryMonth": "3",
+            "expiryYear": "2030",
+            "holderName": "John Smith",
+            "number": "1111"
+          },
+          "contractTypes": [
+            "ONECLICK",
+            "RECURRING"
+          ],
+          "creationDate": "2023-03-31T13:49:24+02:00",
+          "firstPspReference": "GFZ8VJQGWMK2WN82",
+          "name": "Your order number",
+          "networkTxReference": "749859169287547",
+          "paymentMethodVariant": "visa",
+          "recurringDetailReference": "FJNJQH2Z83M84H82",
+          "variant": "visa"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "bank": {
+            "bankName": "Wirecard",
+            "countryCode": "DE",
+            "iban": "DE87123456781234567890",
+            "ownerName": "Simon Hopper"
+          },
+          "contractTypes": [
+            "RECURRING",
+            "PAYOUT"
+          ],
+          "creationDate": "2023-04-05T08:23:45+02:00",
+          "firstPspReference": "G24LPNPDQ4BLNK82",
+          "paymentMethodVariant": "bankTransfer_IBAN",
+          "recurringDetailReference": "ZNDHRJ5FP6KXWD82",
+          "shopperName": {
+            "firstName": "Adyen",
+            "lastName": "Test"
+          },
+          "variant": "bankTransfer_IBAN"
+        }
+      },
+      {
+        "RecurringDetail": {
+          "billingAddress": {
+            "city": "So Paulo",
+            "country": "AT",
+            "houseNumberOrName": "999",
+            "postalCode": "59000060",
+            "stateOrProvince": "SP",
+            "street": "Roque Petroni Jr"
+          },
+          "contractTypes": [
+            "RECURRING"
+          ],
+          "creationDate": "2023-04-05T20:05:12+02:00",
+          "firstPspReference": "ZCKJZKGMWMK2WN82",
+          "paymentMethodVariant": "klarna",
+          "recurringDetailReference": "TC3SBKQ593M84H82",
+          "tokenDetails": {
+            "tokenData": {
+              "klarnapayments.country": "AT",
+              "klarnapayments.txVariant": "klarna",
+              "klarnapayments.shopperEmail": "customer@email.at",
+              "klarnapayments.tokenId": "c5ca59fd-887b-42e2-ab43-e438575f828c"
+            },
+            "tokenDataType": "KlarnaPayments"
+          },
+          "variant": "klarna"
+        }
+      }
     ],
-    "shopperReference": "test-123",
-    "invalidOneclickContracts": "false"
-};
+    "lastKnownShopperEmail": "s.hopper@test.com",
+    "shopperReference": "YOUR_UNIQUE_SHOPPER_ID"
+  };
