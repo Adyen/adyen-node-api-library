1c20099a50b1240b255f58179a6dcaa2adad46a8
jillingk <93914435+jillingk@users.noreply.github.com>
2023-07-07 21:46:47 +0200

    Regenerated transfers

--- src/services/index.ts
+++ src/services/index.ts
@@ -10,7 +10,7 @@ export { default as StoredValue} from "./storedValue";
 export { default as TerminalManagementAPI } from "./terminalManagementApi";
 export { default as Management } from "./management";
 export { default as LegalEntityManagementAPI } from "./legalEntityManagement";
-export { default as Transfers } from "./transfers";
+export { default as TransfersAPI } from "./transfers";
 export { default as BalancePlatform } from "./balancePlatform";
 export { default as DataProtection } from "./dataProtection";
 export { default as BalanceControlAPI } from "./balanceControlApi";
