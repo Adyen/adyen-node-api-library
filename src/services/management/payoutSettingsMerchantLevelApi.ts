5b41a65962f978fd69707fd65ca0beb433582675
AdyenAutomationBot <Adyen Automation plugins_dev@adyen.com>
2024-10-09 20:10:43 +0000

    style(fmt): code formatted

--- src/services/management/payoutSettingsMerchantLevelApi.ts
+++ src/services/management/payoutSettingsMerchantLevelApi.ts
@@ -14,7 +14,6 @@ import {
     PayoutSettings,
     PayoutSettingsRequest,
     PayoutSettingsResponse,
-    RestServiceError,
     UpdatePayoutSettingsRequest,
     ObjectSerializer
 } from "../../typings/management/models";
