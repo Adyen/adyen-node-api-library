fd2b92f3bb1e1609f1f66edd02a1b1f0ec54591a
Beppe Catanese <gcatanese@yahoo.com>
2025-01-21 19:41:15 +0100

    Revert breaking change in v23.3.0

--- src/typings/payment/additionalDataAirline.ts
+++ src/typings/payment/additionalDataAirline.ts
@@ -98,9 +98,9 @@ export class AdditionalDataAirline {
     */
     'airline_passenger_last_name'?: string;
     /**
-    * The passenger\'s phone number, including country code. This is an alphanumeric field that can include the \'+\' and \'-\' signs. * Encoding: ASCII * minLength: 3 characters * maxLength: 30 characters
+    * The passenger\'s telephone number, including country code. This is an alphanumeric field that can include the \'+\' and \'-\' signs. * Encoding: ASCII * minLength: 3 characters * maxLength: 30 characters
     */
-    'airline_passenger_phone_number'?: string;
+    'airline_passenger_telephone_number'?: string;
     /**
     * The IATA passenger type code (PTC). * Encoding: ASCII * minLength: 3 characters * maxLength: 6 characters
     */
@@ -240,8 +240,8 @@ export class AdditionalDataAirline {
             "type": "string"
         },
         {
-            "name": "airline_passenger_phone_number",
-            "baseName": "airline.passenger.phone_number",
+            "name": "airline_passenger_telephone_number",
+            "baseName": "airline.passenger.telephone_number",
             "type": "string"
         },
         {
