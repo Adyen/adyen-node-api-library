51819a6455b991ee0cff12cc474b9ee135bba325
Beppe Catanese <1771700+gcatanese@users.noreply.github.com>
2025-02-27 13:13:20 +0000

    Add tools/hmac

---
+++ tools/hmac/hmacValidatorPayments.js
@@ -0,0 +1,59 @@
+// script to calculate the HMAC signature of Payments webhooks (where the signature is calculated considering
+// a subset of the fields in the payload - i.e. NotificationRequestItem object)
+//
+// Run with: `node hmacValidatorPayments.js {hmacKey} {path to JSON file}
+//
+// cd tools/hmac
+// node hmacValidatorPayments.js 11223344D785FBAE710E7F943F307971BB61B21281C98C9129B3D4018A57B2EB payload.json
+
+const fs = require('fs');
+const { hmacValidator } = require('@adyen/api-library');
+
+// Ensure correct arguments
+if (process.argv.length !== 4) {
+    console.error("Usage: node HMACValidatorPayments.js <hmacKey> <payloadFile>");
+    process.exit(1);
+}
+
+const hmacKey = process.argv[2];
+const payloadFile = process.argv[3];
+
+// Check if file exists
+if (!fs.existsSync(payloadFile)) {
+    console.error(`Error: File '${payloadFile}' not found.`);
+    process.exit(1);
+}
+
+console.log(`Calculating HMAC signature with payload from '${payloadFile}'`);
+
+// Load and parse JSON file
+let jsonData;
+try {
+    const fileContent = fs.readFileSync(payloadFile, 'utf8');
+    jsonData = JSON.parse(fileContent);
+} catch (error) {
+    console.error("Error: Invalid JSON in payload file.");
+    process.exit(1);
+}
+
+// Validate JSON structure
+if (!jsonData.notificationItems || !Array.isArray(jsonData.notificationItems)) {
+    console.error("Error: 'notificationItems' key is missing or not an array.");
+    process.exit(1);
+}
+
+// Extract the first (and only) NotificationRequestItem
+const notificationRequestItem = jsonData.notificationItems[0]?.NotificationRequestItem;
+
+if (!notificationRequestItem) {
+    console.error("Error: 'NotificationRequestItem' is not found.");
+    process.exit(1);
+}
+
+//console.log(notificationRequestItem)
+
+const validator = new hmacValidator()
+const hmacSignature = validator.calculateHmac(notificationRequestItem, hmacKey);
+
+console.log(`HMAC Validation Result: ${hmacSignature}`);
+process.exit(0);
