0c412634158a59a2c3a69eb4d4e75a671b48b0e3
AdyenAutomationBot <Adyen Automation plugins_dev@adyen.com>
2025-02-28 15:36:12 +0000

    style(fmt): code formatted

--- src/services/management/usersMerchantLevelApi.ts
+++ src/services/management/usersMerchantLevelApi.ts
@@ -14,7 +14,6 @@ import {
     CreateMerchantUserRequest,
     CreateUserResponse,
     ListMerchantUsersResponse,
-    RestServiceError,
     UpdateMerchantUserRequest,
     User,
     ObjectSerializer
