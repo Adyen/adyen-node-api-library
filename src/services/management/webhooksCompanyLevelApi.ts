56d5e638a8b123e2ee605709a8a561af042ca562
AdyenAutomationBot <Adyen Automation plugins_dev@adyen.com>
2025-03-05 09:59:21 +0000

    style(fmt): code formatted

--- src/services/management/webhooksCompanyLevelApi.ts
+++ src/services/management/webhooksCompanyLevelApi.ts
@@ -14,7 +14,6 @@ import {
     CreateCompanyWebhookRequest,
     GenerateHmacKeyResponse,
     ListWebhooksResponse,
-    RestServiceError,
     TestCompanyWebhookRequest,
     TestWebhookResponse,
     UpdateCompanyWebhookRequest,
