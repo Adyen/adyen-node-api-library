c6b6722ae73e27619e74f65038aac96aa20c6247
Beppe Catanese <gcatanese@yahoo.com>
2025-05-30 12:18:52 +0200

    Add oneOf tests

---
+++ src/__mocks__/checkout/payments3DS2NativeAction.ts
@@ -0,0 +1,32 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ * Adyen NodeJS API Library
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+/* tslint:disable */
+export const payments3DS2NativeAction = JSON.stringify({
+    "action":{
+        "type":"threeDS2",
+        "subtype": "fingerprint",
+        "paymentData":"Ab02b4c0!BQABAgCuZFJrQOjSsl\/zt+...",
+        "paymentMethodType":"scheme",
+        "authorisationToken" : "Ab02b4c0!BQABAgAvrX03p...",
+        "token":"eyJ0aHJlZURTTWV0aG9kTm90aWZpY..."
+    },
+    pspReference: "JLCMPCQ8HXSKGK82",
+    resultCode:"IdentifyShopper",  
+}, null, 2);
