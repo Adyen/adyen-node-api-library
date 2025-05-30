0b4bd0db4956cd75a071c02f786a4b9c01b8f257
Beppe Catanese <gcatanese@yahoo.com>
2025-05-30 16:01:11 +0200

    Generate PosMobileApi

--- src/typings/posMobile/createSessionRequest.ts
+++ src/typings/posMobile/createSessionRequest.ts
@@ -22,27 +22,35 @@ export class CreateSessionRequest {
     */
     'store'?: string;
 
-    static discriminator: string | undefined = undefined;
+    static readonly discriminator: string | undefined = undefined;
 
-    static attributeTypeMap: Array<{name: string, baseName: string, type: string}> = [
+    static readonly mapping: {[index: string]: string} | undefined = undefined;
+
+    static readonly attributeTypeMap: Array<{name: string, baseName: string, type: string, format: string}> = [
         {
             "name": "merchantAccount",
             "baseName": "merchantAccount",
-            "type": "string"
+            "type": "string",
+            "format": ""
         },
         {
             "name": "setupToken",
             "baseName": "setupToken",
-            "type": "string"
+            "type": "string",
+            "format": ""
         },
         {
             "name": "store",
             "baseName": "store",
-            "type": "string"
+            "type": "string",
+            "format": ""
         }    ];
 
     static getAttributeTypeMap() {
         return CreateSessionRequest.attributeTypeMap;
+    }
+
+    public constructor() {
     }
 }
 
