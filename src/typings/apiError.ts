1589b18dc5481ff8ef1c282ebcd4b18302ed306f
Beppe Catanese <gcatanese@yahoo.com>
2025-06-05 23:28:44 +0200

    Expand ApiError object

--- src/typings/apiError.ts
+++ src/typings/apiError.ts
@@ -17,12 +17,21 @@
  * See the LICENSE file for more info.
  */
 
-// Generated using typescript-generator version 2.14.505 on 2019-06-04 08:51:10.
-
+// ApiError interface to model Adyen API errors
+// The interface is an union of the 2 different error models used by Adyen APIs:
+// RestServiceError (based on RFC 7807)
+// ServiceError (based on the original Adyen error model).
 export interface ApiError {
     status?: number;
     errorCode?: string;
     message?: string;
     errorType?: string;
     pspReference?: string;
+
+    detail?: string
+    instance?: string;
+    invalidFields?: Array<string>;
+    requestId?: string;
+    title?: string;
+    type?: string;
 }
