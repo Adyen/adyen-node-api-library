c75293394325c72daf033157ea2531672fddfa1a
Beppe Catanese <gcatanese@yahoo.com>
2025-06-18 09:35:40 +0200

    Generate signature with required params first

--- src/__tests__/transfers.spec.ts
+++ src/__tests__/transfers.spec.ts
@@ -47,6 +47,45 @@ describe("Transfers", (): void => {
         expect(response.id).toEqual("1W1UG35U8A9J5ZLG");
     });
 
+    test("should get transfer", async (): Promise<void> => {
+        scope.get("/transfers/123")
+        .reply(200, transfersSuccess);
+        const response: transfers.TransferData = await transfersAPI.TransfersApi.getTransfer("123",);
+        expect(response.id).toEqual("1W1UG35U8A9J5ZLG");
+    });
+
+    test("should list transfers", async (): Promise<void> => {
+        const createdSince = new Date(Date.UTC(2023, 11, 12, 0, 0, 0)); // 12-12-2023 December is month 11
+        const createdUntil = new Date(Date.UTC(2023, 11, 13, 0, 0, 0)); // 13-12-2023 December is month 11
+    
+        scope
+          .get("/transfers")
+          .query({
+            balancePlatform: "platform",
+            createdSince: createdSince.toISOString(),
+            createdUntil: createdUntil.toISOString(),
+          })
+          .reply(200, listTransactionsSuccess);
+    
+        const response: transfers.FindTransfersResponse = await transfersAPI.TransfersApi.getAllTransfers(
+            createdSince,
+            createdUntil,
+            "platform",
+            undefined,
+            undefined,
+            undefined,
+            undefined
+        );
+    
+        expect(response.data?.length).toEqual(3);
+        if (response.data && response.data.length > 0) {
+            expect(response.data[0].id).toEqual("1VVF0D5U66PIUIVP");
+        } else {
+            fail();
+        }
+    });
+
+
     test("should get transaction", async (): Promise<void> => {
         scope.get("/transactions/123")
         .reply(200, getTransactionSuccess);
@@ -68,13 +107,13 @@ describe("Transfers", (): void => {
           .reply(200, listTransactionsSuccess);
     
         const response: transfers.TransactionSearchResponse = await transfersAPI.TransactionsApi.getAllTransactions(
+            createdSince,
+            createdUntil,
             "platform",
             undefined,
             undefined,
             undefined,
-            undefined,
-            createdSince,
-            createdUntil
+            undefined
         );
     
         expect(response.data?.length).toEqual(3);
@@ -84,4 +123,5 @@ describe("Transfers", (): void => {
             fail();
         }
     });
+
 });
