9bd105c0fc3b4549f61ed0c544a30802f31f60af
Beppe Catanese <gcatanese@yahoo.com>
2025-07-11 09:58:48 +0200

    Improve test code

--- src/__tests__/httpClientException.spec.ts
+++ src/__tests__/httpClientException.spec.ts
@@ -1,16 +1,14 @@
-import { HttpClientException } from "../"; 
+import { HttpClientException } from "../";
 
 describe("HttpClientException", () => {
   it("should export HttpClientException as a class", () => {
     expect(typeof HttpClientException).toBe("function");
 
     const error = new HttpClientException({
-                    message: `HTTP Exception: `,
-                    statusCode: 422,
-                    errorCode: undefined,
-                    responseHeaders: undefined,
-                    responseBody: "{\"status\": 422, \"message\": \"Test error\"}",
-                });
+      message: "Test error",
+      statusCode: 422,
+      responseBody: JSON.stringify({ status: 422, message: "Test error" }),
+    });
 
     expect(error).toBeInstanceOf(HttpClientException);
     expect(error.statusCode).toBe(422);
