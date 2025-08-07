3e3609d1f9d342af66c2b4aa15d67a0d981bdb50
beppe <beppe.catanese@adyen.com>
2025-08-07 10:32:14 +0200

    Update async signature

--- src/services/terminalCloudAPI.ts
+++ src/services/terminalCloudAPI.ts
@@ -52,11 +52,22 @@ class TerminalCloudAPI extends Service {
         return ObjectSerializer.serialize(request, "TerminalApiRequest");
     }
 
-    public async(terminalApiRequest: TerminalApiRequest): Promise<string> {
+    /**
+     * Send an asynchronous payment request to the Terminal API.
+     *
+     * @param terminalApiRequest - The request to send.
+     * @returns A promise that resolves to "ok" if the request was successful, or a TerminalApiResponse if there is an error.
+     */
+    public async(terminalApiRequest: TerminalApiRequest): Promise<string | TerminalApiResponse> {
         const request = TerminalCloudAPI.setApplicationInfo(terminalApiRequest);
         return getJsonResponse<TerminalApiRequest>(this.terminalApiAsync, request);
     }
 
+    /**
+     * Send a synchronous payment request to the Terminal API.
+     * @param terminalApiRequest - The request to send.
+     * @returns A promise that resolves to a TerminalApiResponse.
+     */
     public async sync(terminalApiRequest: TerminalApiRequest): Promise<TerminalApiResponse> {
         const request = TerminalCloudAPI.setApplicationInfo(terminalApiRequest);
         const response = await getJsonResponse<TerminalApiRequest, TerminalApiResponse>(
