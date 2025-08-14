789e76b7d294e405abffe810d37dfd58d6e854be
AdyenAutomationBot <AdyenAutomationBot 38424300+AdyenAutomationBot@users.noreply.github.com>
2025-08-14 08:11:37 +0000

    style(fmt): code formatted

--- src/services/management/clientKeyCompanyLevelApi.ts
+++ src/services/management/clientKeyCompanyLevelApi.ts
@@ -16,7 +16,6 @@ import Resource from "../resource";
 
 import { ObjectSerializer } from "../../typings/management/objectSerializer";
 import { GenerateClientKeyResponse } from "../../typings/management/models";
-import { RestServiceError } from "../../typings/management/models";
 
 /**
  * API handler for ClientKeyCompanyLevelApi
