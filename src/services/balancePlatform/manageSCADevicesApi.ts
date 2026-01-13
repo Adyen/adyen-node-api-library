4c68d8250bb11bfe1feb6cdc135991b01cd20de1
AdyenAutomationBot <AdyenAutomationBot 38424300+AdyenAutomationBot@users.noreply.github.com>
2026-01-13 13:43:45 +0000

    style(fmt): code formatted

--- src/services/balancePlatform/manageSCADevicesApi.ts
+++ src/services/balancePlatform/manageSCADevicesApi.ts
@@ -22,7 +22,6 @@ import { AssociationInitiateResponse } from "../../typings/balancePlatform/model
 import { RegisterSCAFinalResponse } from "../../typings/balancePlatform/models";
 import { RegisterSCARequest } from "../../typings/balancePlatform/models";
 import { RegisterSCAResponse } from "../../typings/balancePlatform/models";
-import { RestServiceError } from "../../typings/balancePlatform/models";
 import { SearchRegisteredDevicesResponse } from "../../typings/balancePlatform/models";
 
 /**
