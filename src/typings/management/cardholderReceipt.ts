84542dd9822a7787201c394ab04d472e454c7476
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-01-14 14:55:41 +0000

    [management] Automated update from Adyen/adyen-openapi@0a007ce

--- src/typings/management/cardholderReceipt.ts
+++ src/typings/management/cardholderReceipt.ts
@@ -10,7 +10,7 @@
 
 export class CardholderReceipt {
     /**
-    * A custom header to show on the shopper receipt for an authorised transaction. Allows one or two comma-separated header lines, and blank lines. For example, `header,header,filler`
+    * The structure of the header to show on the shopper receipt. You can define the order of one or two header lines and blank lines. For example, **header1,header2,filler**. The text of the header lines is defined in the Customer Area under **In-person payments** > **Terminal settings** > **Receipts** in the **Receipt lines** block.
     */
     "headerForAuthorizedReceipt"?: string;
 
