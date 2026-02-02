b7a8465b873c20f365b131bec844234ea127913d
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-02 09:13:08 +0000

    [balancewebhooks] Automated update from Adyen/adyen-openapi@9c9bc16

--- src/typings/balanceWebhooks/models.ts
+++ src/typings/balanceWebhooks/models.ts
@@ -1,7 +1,11 @@
+export * from "./amount"
 export * from "./balanceAccountBalanceNotificationRequest"
 export * from "./balanceNotificationData"
 export * from "./balancePlatformNotificationResponse"
 export * from "./balances"
+export * from "./releaseBlockedBalanceNotificationData"
+export * from "./releasedBlockedBalanceNotificationRequest"
+export * from "./resourceReference"
 
 // serializing and deserializing typed objects 
 export * from "./objectSerializer"
