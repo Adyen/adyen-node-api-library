6e3b7b2f4a850f1630367a64f4725c42766d1135
beppe <beppe.catanese@adyen.com>
2026-07-07 14:44:25 +0200

    Add test

--- src/__tests__/webhooks/transferWebhook.spec.ts
+++ src/__tests__/webhooks/transferWebhook.spec.ts
@@ -1,5 +1,6 @@
 import { EstimationTrackingData } from "../../typings/transferWebhooks/estimationTrackingData";
 import { TransferWebhooksHandler } from "../../typings/transferWebhooks/transferWebhooksHandler";
+import { USAchTracingData } from "../../typings/transferWebhooks/uSAchTracingData";
 
 describe("Transfer Webhook Serialization", (): void => {
   test("should correctly deserialize union types in transfer webhooks", () => {
@@ -30,6 +31,37 @@ describe("Transfer Webhook Serialization", (): void => {
       expect(
         transferNotification.data.tracking.estimatedArrivalTime
       ).toBeInstanceOf(Date);
+    }
+  });
+
+  test("should correctly deserialize union types for tracing in transfer webhooks", () => {
+    // Simplest possible webhook with just the fields we need to test
+    const webhookData = {
+      data: {
+        id: "test-transfer-id",
+        tracing: {
+          traceNumber: "123456789012345",
+          type: "usAch",
+        },
+      },
+      type: "balancePlatform.transfer.updated",
+    };
+
+    const jsonString = JSON.stringify(webhookData);
+    const transferWebhooksHandler = new TransferWebhooksHandler(jsonString);
+    const transferNotification =
+      transferWebhooksHandler.getTransferNotificationRequest();
+
+    if (transferNotification.data.tracing?.type === "usAch") {
+      // Verify that the tracing object is properly deserialized to the correct type
+      expect(transferNotification.data.tracing).toBeInstanceOf(
+        USAchTracingData
+      );
+
+      // Verify that traceNumber is properly deserialized
+      expect(transferNotification.data.tracing.traceNumber).toBe(
+        "123456789012345"
+      );
     }
   });
 });
