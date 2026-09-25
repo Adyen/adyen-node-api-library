6999bc837f0d796fa19a4ab9d20a463cfecc7517
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-09-25 11:05:22 +0000

    [foreignexchange] Automated update from Adyen/adyen-openapi@e96f4b3

---
+++ src/typings/foreignExchange/models.ts
@@ -0,0 +1,12 @@
+export * from "./amount"
+export * from "./calculateRateRequest"
+export * from "./calculateRateRequestItem"
+export * from "./calculateRateResponse"
+export * from "./calculateRateResponseItem"
+export * from "./defaultErrorResponseEntity"
+export * from "./exchangeSide"
+export * from "./invalidField"
+export * from "./rateType"
+
+// serializing and deserializing typed objects 
+export * from "./objectSerializer"
